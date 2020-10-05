655e4fe867e6941febf6ba8a9b40054273d0e7af
simonihmig <simon.ihmig@kaliber5.de>
2020-10-05 10:30:44 +0200

    Refactor Tooltips and Popovers to Glimmer components

--- addon/utils/decorators/arg.js
+++ addon/utils/decorators/arg.js
@@ -1,15 +1,21 @@
 import { macroCondition, getOwnConfig } from '@embroider/macros';
 
 export default function arg(target, key, descriptor) {
-  let defaultValue = descriptor.initializer ? descriptor.initializer() : undefined;
-
   return {
     get() {
       if (macroCondition(getOwnConfig().useDefaultValueIfUndefined)) {
         const argValue = this.args[key];
-        return argValue !== undefined ? argValue : defaultValue;
+        return argValue !== undefined
+          ? argValue
+          : descriptor.initializer
+          ? descriptor.initializer.call(this)
+          : undefined;
       } else {
-        return Object.keys(this.args).includes(key) ? this.args[key] : defaultValue;
+        return Object.keys(this.args).includes(key)
+          ? this.args[key]
+          : descriptor.initializer
+          ? descriptor.initializer.call(this)
+          : undefined;
       }
     },
   };
